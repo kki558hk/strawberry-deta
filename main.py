7aa2048610ec7f04f59309d45ee3eead7fd3ebf4
kki558hk <127036163+kki558hk@users.noreply.github.com>
2023-05-07 09:55:04 +0800

    Update main.py
    HTTP オプションメソッドのでエラーになったため

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from schemas.graphqlBase import schema,graphql_app
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 app.include_router(graphql_app, prefix="/graphql")
-
+app.add_middleware(
+    CORSMiddleware, allow_headers=["*"], allow_origins=["*"], allow_methods=["*"]
+)
 
